20c40ffc6d5efd9a2e6026ea887ac60ae78b4a54
Alex McLean <bigals992@gmail.com>
2022-12-04 00:45:10 -0500

    Day 4 solutions complete

--- Day04/day-4-app/src/use/day4Composition.ts
+++ Day04/day-4-app/src/use/day4Composition.ts
@@ -4,13 +4,46 @@ export default function day4Composition() {
   const inputData = inputFile.split("\n");
 
   const day4FirstSolution = () => {
-    /* First Problem Solution goes here */
-    return '';
+    let numOfOverlaps = 0;
+
+    for (const line of inputData) {
+      const firstGroup = line.split(",")[0];
+      const firstGroupStart = firstGroup.split("-")[0];
+      const firstGroupEnd = firstGroup.split("-")[1];
+      const secondGroup = line.split(",")[1];
+      const secondGroupStart = secondGroup.split("-")[0];
+      const secondGroupEnd = secondGroup.split("-")[1];
+
+      const extremes = `${Math.min(
+        firstGroupStart,
+        secondGroupStart
+      )}-${Math.max(firstGroupEnd, secondGroupEnd)}`;
+
+      if (firstGroup === extremes || secondGroup === extremes) {
+        numOfOverlaps++;
+      }
+    }
+
+    return numOfOverlaps;
   };
 
   const day4SecondSolution = () => {
-    /* Second Problem Solution goes here */
-    return '';
+    let numOfOverlaps = 0;
+
+    for (const line of inputData) {
+      const [[first1, second1], [first2, second2]] = line
+        .split(",")
+        .map((range: string) => range.split("-").map(Number));
+
+      if (
+        (first1 >= first2 && first1 <= second2) ||
+        (first2 >= first1 && first2 <= second1)
+      ) {
+        numOfOverlaps++;
+      }
+    }
+
+    return numOfOverlaps;
   };
 
   return { day4FirstSolution, day4SecondSolution };

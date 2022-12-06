3b2b34b3ccaf9ac39e86d798ea3820fef73bea4a
Alex McLean <bigals992@gmail.com>
2022-12-06 09:10:54 -0500

    Day-06 solutions complete, not the most effiecient but it works

--- Day06/day-6-app/src/use/day6Composition.ts
+++ Day06/day-6-app/src/use/day6Composition.ts
@@ -4,13 +4,65 @@ export default function day6Composition() {
   const inputData = inputFile.split("\n");
 
   const day6FirstSolution = () => {
-    /* First Problem Solution goes here */
-    return '';
+    let count = 0;
+    const chars = inputData[0].split("");
+    let markerSet = [];
+
+    for (let i = 0; i < chars.length && markerSet.length < 4; i++) {
+      const char = chars[i];
+
+      if (
+        markerSet?.[0] === char ||
+        markerSet?.[1] === char ||
+        markerSet?.[2] === char ||
+        markerSet?.[3] === char
+      ) {
+        markerSet = [char];
+      } else {
+        markerSet.push(char);
+      }
+
+      count++;
+    }
+    count--;
+
+    return count;
   };
 
   const day6SecondSolution = () => {
-    /* Second Problem Solution goes here */
-    return '';
+    let count = 0;
+    const chars = inputData[0].split("");
+    let markerSet = [];
+
+    for (let i = 0; i < chars.length && markerSet.length < 14; i++) {
+      const char = chars[i];
+
+      if (
+        markerSet?.[0] === char ||
+        markerSet?.[1] === char ||
+        markerSet?.[2] === char ||
+        markerSet?.[3] === char ||
+        markerSet?.[4] === char ||
+        markerSet?.[5] === char ||
+        markerSet?.[6] === char ||
+        markerSet?.[7] === char ||
+        markerSet?.[8] === char ||
+        markerSet?.[9] === char ||
+        markerSet?.[10] === char ||
+        markerSet?.[11] === char ||
+        markerSet?.[12] === char ||
+        markerSet?.[13] === char
+      ) {
+        markerSet = [char];
+      } else {
+        markerSet.push(char);
+      }
+
+      count++;
+    }
+    count--;
+
+    return count;
   };
 
   return { day6FirstSolution, day6SecondSolution };
